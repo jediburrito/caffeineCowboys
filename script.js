67d2c6e4299f5f5aa2f1c6418734fb7ff9ec4dbf
jediburrito <jediburrito@gmail.com>
2020-01-12 21:03:33 -0500

    first upload. Webpage for login/create profile

---
+++ script.js
@@ -0,0 +1,19 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add('right-panel-active');
+});
+
+signInButton.addEventListener('click', () =>{
+	container.classList.remove('right-panel-active');
+});
+
+document.getElementById('loginBtn').addEventListener('click', ()=>{
+	alert("Login button works!");
+});
+
+document.getElementById('createBtn').addEventListener('click', ()=>{
+	alert("Create Profile button works!");
+});
