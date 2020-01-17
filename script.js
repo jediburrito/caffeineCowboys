294c4617df2f8e75534fed5b2d6b0ad128978dcb
jediburrito <jediburrito@gmail.com>
2020-01-17 17:13:07 -0500

    ver 1.0

--- script.js
+++ script.js
@@ -10,15 +10,58 @@ signInButton.addEventListener('click', () =>{
 	container.classList.remove('right-panel-active');
 });
 
-document.getElementById('loginBtn').addEventListener('click', ()=>{
-	if(document.getElementById('email1').value.length == 0) alert("email empty!");
-	else if(document.getElementById('password1').value.length == 0) alert("password empty!");
-	else alert("email: " + document.getElementById('email1').value + "\n password: " + document.getElementById('password1').value);
-});
+document.getElementById('loginBtn').addEventListener('click', ()=> loginButton());
+document.getElementById('createBtn').addEventListener('click', ()=> createAccountBtn());
 
-document.getElementById('createBtn').addEventListener('click', ()=>{
-	if(document.getElementById('name').value.length == 0) alert("name empty!");
-	else if(document.getElementById('email2').value.length == 0) alert("email empty!");
-	else if(document.getElementById('password2').value.length == 0) alert("password empty!");
-	else alert("name: \n" + document.getElementById('email2').value + "email: \n" + document.getElementById('email2').value + "\n password: " + document.getElementById('password2').value);
-});
+function dbQuery(value) {
+
+}
+
+function createAccount(name, email, pass) {
+
+	let accObj = {name: name, email: email, password: pass};
+}
+
+function login(email, pass) {
+
+}
+
+function checkParam(field , param) {
+	if(param.length == 0) {
+		alert(field + " field empty!");
+		return 1;
+	}
+	if(param.length > 29 ) {
+		alert(field + " field too many characters!");
+		return 2;
+	}
+
+	return 0;
+}
+
+function loginButton() {
+	let errCheck = 0;
+	let email = document.getElementById('email1').value;
+	if(checkParam("email" , email) != 0)
+		errCheck = 1;
+	let pass = document.getElementById('password1').value;
+	if(checkParam("password" , pass) != 0)
+		errCheck = 1;
+
+	(errCheck == 0) ? alert("\nemail: " + email + "\n password: " + pass) : alert("please check fields and try again.");
+}
+
+function createAccountBtn (){
+	let errCheck = 0;
+	let name = document.getElementById('name').value;
+	if(checkParam("name", name) != 0)
+		errCheck = 1;
+	let email = document.getElementById('email2').value;
+	if(checkParam("email" , email) != 0)
+		errCheck = 1;
+	let pass = document.getElementById('password2').value;
+	if(checkParam("password" , pass) != 0)
+		errCheck = 1;
+
+	(errCheck == 0) ? alert("name: " + name + "\nemail: " + email + "\n password: " + pass) : alert("please check fields and try again.");
+}
